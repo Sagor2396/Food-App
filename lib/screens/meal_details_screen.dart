044b68e5ccd0966b976fede13e73b3a7a768692c
princ <sagorsarkar2396@gmail.com>
2024-05-18 02:33:59 +0600

    showing content of meal_details_screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/data/dummy_data.dart';
 import '../models/meal.dart';
 
-class MealDetailsScreen extends StatelessWidget{
+class MealDetailsScreen extends StatelessWidget {
   MealDetailsScreen({super.key, required this.meal});
 
   final Meal meal;
@@ -12,9 +13,59 @@ class MealDetailsScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl,
-      width: 350,height: 400,fit: BoxFit.cover,),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            SizedBox(height: 12,),
+            Text('Ingredients',textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.copyWith(color: Theme.of(context).primaryColor)),
+            SizedBox(height: 10,),
+            ...meal.ingredients.map((ingredient) {
+              return Padding(
+                padding:
+                const EdgeInsets.symmetric(vertical: 1, horizontal: 30),
+                child: Text(
+                  ingredient,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).primaryColorDark),
+                ),
+              );
+            }).toList(),
+            SizedBox(height: 20,),
+            Text('Steps',
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.copyWith(color: Theme.of(context).primaryColor)),
+            SizedBox(height: 10,),
+            ...meal.steps.map((step){
+              return Padding(
+                padding:
+                const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).primaryColorDark),
+                ),
+              );
+            }).toList(),
+        
+          ],
+        ),
+      ),
     );
   }
-
 }
--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -4,20 +4,28 @@ import 'package:food_app/screens/meal_details_screen.dart';
 import 'package:food_app/widgets/meals_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  MealsScreen({super.key, required this.title, required this.meals,});
+  MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
 
   String title;
   List<Meal> meals;
 
-  void selectMeal(BuildContext context, Meal meal){
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=> MealDetailsScreen(meal: meal)));
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (ctx) => MealDetailsScreen(meal: meal)));
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (context, index) => MealsItem(meal: meals[index], onSelectMeal: (meal) => selectMeal(context,meal),),
+      itemBuilder: (context, index) => MealsItem(
+        meal: meals[index],
+        onSelectMeal: (meal) => selectMeal(context, meal),
+      ),
     );
 
     if (meals.isEmpty) {
